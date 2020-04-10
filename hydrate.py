5a7b608bc2c28abcc6331d2cd7ea04301bcb29e1
dunyaoguz <dunya.oguz@mail.mcgill.ca>
2020-04-10 19:56:42 -0400

    add script to check for data quality and perform data cleaning

--- hydrate.py
+++ hydrate.py
@@ -12,6 +12,8 @@ import os
 import re
 import boto3
 
+from io import StringIO
+
 import pandas as pd
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
@@ -23,40 +25,33 @@ twitter_access = os.environ['twitter_access']
 twitter_secret = os.environ['twitter_secret']
 aws_access = os.environ['aws_access']
 aws_secret = os.environ['aws_secret']
+data_lake_name = os.environ['data_lake_name']
 
 twarc = Twarc(consumer_key, consumer_secret, twitter_access, twitter_secret)
 
-s3 = boto3.client('s3',
+s3 = boto3.resource('s3',
                   region_name='us-west-2',
                   aws_access_key_id=aws_access,
                   aws_secret_access_key=aws_secret)
 
-# s3.create_bucket(Bucket='covid19-tweets-dunyaoguz', CreateBucketConfiguration={'LocationConstraint':'us-west-2'})
-
 def get_tweet_count():
     '''Computes how many COVID-related tweets were published in a day'''
 
-    tweet_count = pd.DataFrame({'date': [], 'number_of_tweets': []})
+    tweet_count = pd.DataFrame({'file': [], 'number_of_tweets': []})
 
-    data_dirs = ['data/2020-01', 'data/2020-02', 'data/2020-03']
+    for path in Path('tweet_ids').iterdir():
+        if path.name.endswith('.csv'):
 
-    for data_dir in data_dirs:
-        for path in Path(data_dir).iterdir():
-            if path.name.endswith('.txt'):
-                date = re.findall(r'2020-[0-9]{2}-[0-9]{2}', path.name)[0]
+            with open(path, 'r') as f:
+                tweet_ids = f.read()
+                tweet_ids = tweet_ids.split('\n')
 
-                with open(path, 'r') as f:
-                    tweet_ids = f.read()
-                    tweet_ids = tweet_ids.split('\n')
+                tweet_ids.remove('')
+                no_tweets = len(tweet_ids)
 
-                    tweet_ids.remove('')
-                    no_tweets = len(tweet_ids)
+            tweet_count = tweet_count.append(pd.DataFrame({'file': [path], 'number_of_tweets': [no_tweets]}))
 
-                tweet_count = tweet_count.append(pd.DataFrame({'date': [date], 'number_of_tweets': [no_tweets]}))
-
-    tweet_count['date'] = pd.to_datetime(tweet_count['date'])
-    tweet_count = tweet_count.groupby('date')[['number_of_tweets']].sum().reset_index()
-    tweet_count.to_csv('tweet_count.csv')
+    tweet_count.to_csv('tweet_count.csv', index=False)
 
 def extract_tweet_info(tweet, df):
     '''Extracts information related to a given tweet'''
@@ -111,11 +106,13 @@ def main(df, file):
         count += 1
         print(f'Processed {count} tweets')
 
-        users_df.to_csv(f'hydrated_tweets/users_{file}')
-        tweets_df.to_csv(f'hydrated_tweets/{file}')
+        csv_buffer = StringIO()
+        users_df.to_csv(csv_buffer)
+        s3.Object(data_lake_name, f'users_{file}').put(Body=csv_buffer.getvalue())
 
-        s3.upload_file(f'hydrated_tweets/users_{file}', 'covid19-tweets-dunyaoguz', f'users_{file}')
-        s3.upload_file(f'hydrated_tweets/{file}', 'covid19-tweets-dunyaoguz', f'{file}')
+        csv_buffer = StringIO()
+        tweets_df.to_csv(csv_buffer)
+        s3.Object(data_lake_name, f'{file}').put(Body=csv_buffer.getvalue())
 
 if __name__ == "__main__":
     # get_tweet_count()
---
+++ staging_transform.py
@@ -0,0 +1,155 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+
+import boto3
+import pandas as pd
+
+from datetime import datetime
+
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+load_dotenv()
+
+aws_access = os.environ['aws_access']
+aws_secret = os.environ['aws_secret']
+data_lake_name = os.environ['data_lake_name']
+
+s3 = boto3.client('s3',
+                  region_name='us-west-2',
+                  aws_access_key_id=aws_access,
+                  aws_secret_access_key=aws_secret)
+
+def get_objects():
+    '''Get list objects from data lake'''
+    objects = s3.list_objects(Bucket=data_lake_name)['Contents']
+
+    case_data = [object['Key'] for object in objects if 'COVID-19' in object['Key']]
+    tweet_data = [object['Key'] for object in objects if 'covid_tweets' in object['Key'] and 'users' not in object['Key']]
+    twitter_user_data = [object['Key'] for object in objects if 'users_GSU' in object['Key']]
+
+    return {'case_data': case_data, 'tweet_data': tweet_data, 'twitter_user_data': twitter_user_data}
+
+def process_tweet_data(bucket_contents):
+    '''Perform data quality check and formatting fixes for tweet data'''
+
+    df = pd.DataFrame({'tweet_id': [], 'created_at': [], 'full_text': [], 'created_by': [], 'language': [],
+                       'retweet_count': [], 'favorite_count': [], 'in_reply_to_status_id': [], 'in_reply_to_user_id': []})
+
+    retval = {}
+    for tweet_data in bucket_contents['tweet_data']:
+        object = s3.get_object(Bucket= data_lake_name, Key= tweet_data)
+        data = pd.read_csv(object['Body'])
+
+        retval[tweet_data] = data.shape[0]
+        df = df.append(data, sort=False)
+
+    # check number of null values, drop rows that have nulls in certain columns and impute nulls
+    print('---------------------------------------------')
+    print('---------------- NULL VALUES ----------------')
+    print('---------------------------------------------')
+    print(df.isna().sum())
+    for col in ['language', 'created_by', 'full_text']:
+        df.drop(df[df[col].isna()].index, inplace=True)
+    df[['retweet_count', 'favorite_count']] = df[['retweet_count', 'favorite_count']].fillna(0)
+
+    # check for duplicates
+    print('---------------------------------------------')
+    print('------------- DUPLICATE TWEETS --------------')
+    print('---------------------------------------------')
+    print(df.duplicated().sum())
+    df.drop_duplicates(inplace=True)
+
+    # check for number of tweets that weren't able to be hydrated
+    print('---------------------------------------------')
+    print('-------------- UNFOUND TWEETS ---------------')
+    print('---------------------------------------------')
+    original_tweet_counts = pd.read_csv('tweet_count.csv')
+    for _, row in original_tweet_counts.iterrows():
+        print('file:', row.file.split('/')[1])
+        print('percent hydrated:', retval[row.file.split('/')[1]] / row.number_of_tweets)
+
+    # check if the data types are correct
+    print('---------------------------------------------')
+    print('---------------- DATA TYPES -----------------')
+    print('---------------------------------------------')
+    print(df.head())
+    print(df.dtypes)
+
+    # remove milli seconds
+    df.created_at = df.created_at.str[0:19]
+
+    # fix dates
+    dt_converter = lambda x: datetime.strptime('2020 ' + x.strip(), '%Y %a %b %d %X')
+    df['created_at'] = df['created_at'].apply(dt_converter)
+
+    # fix integer columns
+    df['tweet_id'] = df['tweet_id'].apply(int)
+    df['created_by'] = df['created_by'].apply(int)
+    df['retweet_count'] = df['retweet_count'].apply(int)
+    df['favorite_count'] = df['favorite_count'].apply(int)
+
+    df['in_reply_to_user_id'] = df['in_reply_to_user_id'].fillna(0).apply(int)
+    df['in_reply_to_status_id'] = df['in_reply_to_status_id'].fillna(0).apply(int)
+    df[['in_reply_to_status_id', 'in_reply_to_user_id']] = df[['in_reply_to_status_id', 'in_reply_to_user_id']].replace({0:None})
+
+    return df
+
+def process_user_data(bucket_contents):
+    '''Perform data quality check and formatting fixes for twitter user data'''
+
+    df = pd.DataFrame({'user_id': [], 'name': [], 'screen_name': [], 'location': [], 'description': [], 'verified': [],
+                       'followers_count': [], 'friends_count': [], 'statuses_count': [], 'created_at': []})
+
+    for user_data in bucket_contents['twitter_user_data']:
+        object = s3.get_object(Bucket= data_lake_name, Key= user_data)
+        data = pd.read_csv(object['Body'])
+        df = df.append(data, sort=False)
+
+    # check for duplicates
+    print('---------------------------------------------')
+    print('------------- DUPLICATE USERS ---------------')
+    print('---------------------------------------------')
+    print(df.duplicated().sum())
+    df.drop_duplicates(inplace=True)
+
+    # check number of null values, drop rows that have nulls in certain columns
+    print('---------------------------------------------')
+    print('---------------- NULL VALUES ----------------')
+    print('---------------------------------------------')
+    print(df.isna().sum())
+
+    for col in ['user_id', 'created_at', 'verified', 'statuses_count', 'friends_count', 'followers_count']:
+        df.drop(df[df[col].isna()].index, inplace=True)
+
+    # check if the data types are correct
+    print('---------------------------------------------')
+    print('---------------- DATA TYPES -----------------')
+    print('---------------------------------------------')
+    print(df.head())
+    print(df.dtypes)
+
+    df['user_id'] = df['user_id'].apply(int)
+    bool_parser = lambda x: True if x == 1 else False
+
+    df['verified'] = df['verified'].apply(bool_parser)
+
+    df['followers_count'] = df['followers_count'].apply(int)
+    df['friends_count'] = df['friends_count'].apply(int)
+    df['statuses_count'] = df['statuses_count'].apply(int)
+
+    # remove milli seconds
+    df.created_at = df.created_at.str[0:19]
+
+    # fix dates
+    dt_converter = lambda x: datetime.strptime('2020 ' + x.strip(), '%Y %a %b %d %X')
+    df['created_at'] = df['created_at'].apply(dt_converter)
+
+    return df
+
+def main():
+    bucket_contents = get_objects()
+
+    tweets = process_twitter_data(bucket_contents)
+    users = process_user_data(bucket_contents)
+
+if __name__ == "__main__":
+    main()
