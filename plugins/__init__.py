356f4a4e134a88fae32dfaeb08d8276734f23415
dunyaoguz <dunya.oguz@mail.mcgill.ca>
2020-04-12 22:44:15 -0400

    add a custom airflow operator to copy data from s3

---
+++ plugins/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import division, absolute_import, print_function
+
+from airflow.plugins_manager import AirflowPlugin
+
+import operators
+
+class CustomPlugins(AirflowPlugin):
+    name = "custom_plugins"
+
+    operators = [
+        operators.S3ToRedshiftOperator
+    ]
---
+++ plugins/operators/__init__.py
@@ -0,0 +1,3 @@
+from operators.copy_csv_from_s3 import S3ToRedshiftOperator
+
+__all__ = ['S3ToRedshiftOperator']
